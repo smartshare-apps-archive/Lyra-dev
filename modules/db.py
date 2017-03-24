faef01fa4ca4864b33ad7b05ccf08ff7902abd9b
luke lombardi <llom2600@new-host-2.home>
2017-03-24 17:18:30 -0400

    FAQ template

--- modules/db.py
+++ modules/db.py
@@ -11,7 +11,7 @@ default_db = "smartshare-site"
 
 def db_handle():
 	try:
-		conn=MySQLdb.connect(host=REMOTE_HOST,port=REMOTE_PORT,user=USERNAME,passwd=PASSWORD, db=default_db)
+		conn=MySQLdb.connect(host=REMOTE_HOST,port=REMOTE_PORT,user=USERNAME,passwd=PASSWORD, db=default_db, use_unicode=True, charset="utf8")
 		#conn.text_factory = str
 	except Exception as e:
 		print "Exception connecting: ", e
