e9c60c93ae0d4a7afdff14ce7ca2f4d8e440bb1f
Luke Lombardi <llombar1@binghamton.edu>
2017-04-05 18:32:14 -0400

    new template for signup banner

--- modules/control_views.py
+++ modules/control_views.py
@@ -924,6 +924,8 @@ class ControlPanel(object):
 
 		plot = figure(responsive=True)
 		plot.logo = None #removes bokeh logo
+		plot.xaxis.axis_label = 'Day'
+		plot.yaxis.axis_label = 'Sales'
 
 		plot.line([1,2,3,4,5,6,7,8], [3,4,1,6,1,8,1,9])
 
