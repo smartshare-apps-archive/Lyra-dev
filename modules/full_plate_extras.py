90421f4af7d0641d6d8b4949762640f3f69f23d2
Luke Lombardi <llombar1@binghamton.edu>
2017-05-31 20:52:39 -0400

    removed some extra files

--- modules/full_plate_extras.py
+++
@@ -1,5 +0,0 @@
-		#products = loadAllProducts(self.database)
-		#image_resources = loadProductImages(products, self.database)
-		
-		#content_row = render_template("store/content_row_lulu.html")
-		#self.store_data["content_row"] = content_row
